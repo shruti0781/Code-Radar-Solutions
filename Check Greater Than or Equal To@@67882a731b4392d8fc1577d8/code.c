db46e039019cec020891eea5fb680f4221060d02
shruti0781 <195850021+shruti0781@users.noreply.github.com>
2025-01-24 11:22:25 +0530

    Solution to Check Greater Than or Equal To

---
+++ Check Greater Than or Equal To@@67882a731b4392d8fc1577d8/code.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    int a,b;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    {
+        if (a>b)and(a=b){
+            printf("True");
+        }
+        else{
+            printf("False");
+        }
+    }
+}
