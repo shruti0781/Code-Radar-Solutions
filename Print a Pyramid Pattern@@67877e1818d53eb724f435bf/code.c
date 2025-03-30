a903b956af7c8d1cc2a70444505365a614be711a
shruti0781 <shruti0781.becse24@chitkara.edu.in>
2025-03-30 13:32:40 +0530

    Solution to Print a Pyramid Pattern

--- Print a Pyramid Pattern@@67877e1818d53eb724f435bf/code.c
+++ Print a Pyramid Pattern@@67877e1818d53eb724f435bf/code.c
@@ -1,28 +1,19 @@
-// Your code here...
 #include<stdio.h>
-int factorial(int n){
-int fact=1;
-for(int i=2;i<=n;i++)
+int main()
 {
-    fact*=i;
-}
-return fact;
-}
-int combination(int n,int r){
-    int comb;
-    comb= factorial(n)/(factorial(n-r)*factorial(r));
-    return comb;
-}
-int main(){
-    int a;
-    scanf("%d",&a);
-    for(int i=1;i<=a;i++){
-        for(int k=0;k<a-i;k++){
-            printf(" ");
-        }
-        for(int j=1;j<=i+1;j++){
-            printf("*");
+    int i,j,n;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++){
+        for(j=1;j<=2*n-1;j++){
+            if(j<=(n+1-i)&&j>=(n+i-1)){
+                printf("* ");
+            }
+            else{
+                printf(" ");
+
+            }
         }
         printf("\n");
     }
+   
 }
