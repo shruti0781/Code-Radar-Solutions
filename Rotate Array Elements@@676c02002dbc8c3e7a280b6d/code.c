2f4e64510505d8a08ed374baa77385e259f9065d
shruti0781 <shruti0781.becse24@chitkara.edu.in>
2025-03-30 23:23:01 +0530

    Solution to Rotate Array Elements

--- Rotate Array Elements@@676c02002dbc8c3e7a280b6d/code.c
+++ Rotate Array Elements@@676c02002dbc8c3e7a280b6d/code.c
@@ -2,21 +2,34 @@
 
 int main()
 {
-   int temp,i,n,shift,l;
-   scanf("%d",&n);
-   int a[n];
-   for(l=1;l<=n;l++){
-    scanf("%d",&a[l]);
-   }
-   while(shift){
-   temp=a[n-1];
-   for(i=n-1;i>=1;i--){
-       a[i]=a[i-1];
-   }
-   a[0]=temp;
+   int temp, i, n, shift, l;
    
-   shift--;
-}
-for(i=0;i<=n-1;i++)
-   printf("%d",a[i]);
+   // Input the array size and shift count
+   scanf("%d", &n);
+   scanf("%d", &shift);
+
+   int a[n];
+
+   // Corrected input loop (indexing starts from 0)
+   for (l = 0; l < n; l++) {
+       scanf("%d", &a[l]);
+   }
+
+   // Perform right shifts
+   while (shift) {
+       temp = a[n - 1];
+
+       for (i = n - 1; i >= 1; i--) {
+           a[i] = a[i - 1];
+       }
+       a[0] = temp;
+       shift--;
+   }
+
+   // Print the shifted array with spaces
+   for (i = 0; i < n; i++) {
+       printf("%d ", a[i]);
+   }
+
+   return 0;
 }
